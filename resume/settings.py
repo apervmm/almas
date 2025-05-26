595942b93630d51d53f1318eecbca58a2b1acd63
apervmm <apervmm@gmail.com>
2025-05-26 04:01:45 -0700

    upd

--- resume/settings.py
+++ resume/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Production: False
-# Developmmant: True
+# Dev: True
 DEBUG = False
 
 ALLOWED_HOSTS = ['*','.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
